a9b340f13940ea5098093877c793f5bf96942294
cinziamagnani <cinziamagnani@users.noreply.github.com>
2021-01-25 14:27:03 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>
       Weather App
       </h1>
+      <button type="button" class="btn btn-primary">Primary</button>
       <footer>
       <a href="https://github.com/cinziamagnani/app-weather-react" target="_blank" rel="noreferrer">
         Open-source code
@@ -14,6 +17,7 @@ export default function App() {
         Cinzia Magnani
         </a>
         </footer>
+        </div>
     </div>
   );
 }
