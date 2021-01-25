1c4ff73bb98b79e8511d037cb2919543e722a4a6
cinziamagnani <cinziamagnani@users.noreply.github.com>
2021-01-25 13:52:24 +0100

    added GitHub repository url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <h1>
       Weather App
+      </h1>
+      <footer>
+      <a href="https://github.com/cinziamagnani/app-weather-react" target="_blank" rel="noreferrer">
+        Open-source code
+      </a> by 
+      <a href="https://cinziamagnani.com/" target="_blank" rel="noreferrer">
+        Cinzia Magnani
+        </a>
+        </footer>
     </div>
   );
 }
